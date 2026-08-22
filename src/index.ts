7863a85e7df0fa1defc96cc47286910d797a3779
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-08-21 19:34:24 -0500

    Init backend

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// import { envs } from './config/envs';
+// import { AppRoutes } from './presentation/routes';
+// import { Server } from './presentation/server';
+
+
+(async()=> {
+  main();
+})();
+
+
+function main() {
+
+  console.log('Servidor...');
+}
